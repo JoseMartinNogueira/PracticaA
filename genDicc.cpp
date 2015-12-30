315316ea706ada32db40e3fad767da7893d92eb9
JoseMartinNogueira <josemn15@hotmail.com>
2015-12-30 20:12:27 +0100

    a

---
+++ genDicc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <time>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> d;
+	int r;
+	bool b = false;
+	while(d.size != n) {
+		r = rand()%999999;
+		for(int i = 0; i < d.size() and not b; ++i) {
+			if(r == d[i]) b = true;
+		}
+		if (not b) d.push_back();
+		b = false;
+	}
+	for(int i = 0; i < n; ++i) cout >> d[i] >> endl;
+}
