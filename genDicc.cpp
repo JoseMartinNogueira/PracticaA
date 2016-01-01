157af750caceed8741308207119ba92b0eef1d46
JoseMartinNogueira <josemn15@hotmail.com>
2016-01-01 19:40:15 +0100

    falta probarlos

--- genDicc.cpp
+++ genDicc.cpp
@@ -3,11 +3,55 @@
 #include <vector>
 using namespace std;
 
+
+void fusiona(vector<int>& v, int e, int m, int d) {
+    int n = d - e + 1;
+    vector<int> aux(n);
+    int i = e;
+    int j = m + 1;
+    int k = 0;
+    // Inv: aux[0..k-1] es la fusio de v[e..i-1] i v[m+1..j-1]
+    while (i <= m and j <= d) {
+        if (v[i] <= v[j]) {
+            aux[k] = v[i];
+            ++i;
+        }
+        else {
+            aux[k] = v[j];
+            ++j;
+        }
+        ++k;
+    }
+    while (i <= m) {
+        aux[k] = v[i];
+        ++k;
+        ++i;
+    }
+    while (j <= d) {
+        aux[k] = v[j];
+        ++k;
+        ++j;
+    }
+    for (k = 0; k < n; ++k) v[k + e] = aux[k];
+}
+
+// Pre: 0<=e<=d<v.size()
+// Post: els elements de v[e..d] son els inicials, pero ordenats creixentment
+void ordena_per_fusio(vector<int>& v, int e, int d) {
+    if (e < d) {
+        int m = (e + d)/2;
+        ordena_per_fusio(v, e, m);
+        ordena_per_fusio(v, m + 1, d);
+        fusiona(v, e, m, d);
+    }
+}
+
+
 int main() {
 	int n;
 	cin >> n;
-	vector<int> d;
 	int r;
+	vector<int> d;
 	bool b = false;
 	while(d.size != n) {
 		r = rand()%999999;
@@ -17,5 +61,7 @@ int main() {
 		if (not b) d.push_back();
 		b = false;
 	}
+	ordena_per_fusio(d,0,d.size()-1);
 	for(int i = 0; i < n; ++i) cout << d[i] << endl;
 }
+
--- genTex.cpp
+++ genTex.cpp
@@ -8,9 +8,18 @@ int main() {
 	cerr << "Introduzca la n: ";
 	cin >> n;
 	n *= 2;
-	int r;
-	for (int i = 0; i < n; ++i) {
-		r = rand()%999999;
-		cout << r << endl;
+	int propdicc = rand()%n;
+	bool end();
+	int vd = 0;
+	int vnd = 0;
+	while (vd != propdicc and vnd != (n-propdicc)) {
+		if((rand()%2) == 0) {
+			cout >> rand()%propdicc;
+			++vd;
+		}
+		else {
+			++vnd;
+			cout >> rand()%999999;
+		}
 	}
 }
