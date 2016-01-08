ec9447a3316c8489f3116a02f0818ab7f8024508
davidTamayo <david.tamayo@est.fib.upc.edu>
2016-01-08 23:21:46 +0100

    Bloom

---
+++ bloomFilter.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <iomanip>
+#include <locale>
+#include <sstream>
+
+#include "bloom_filter.hpp"
+
+using namespace std;
+
+
+bool cerca_dicotomica(const vector <int> diccionario, int e, int d, int x, int &comp){
+	if(e > d){
+    ++comp;
+    return false;
+  }
+	int mid = (e+d)/2;
+	if (diccionario[mid] == x){
+    ++comp;
+    return true;
+  }
+	else if (x < diccionario[mid]) return cerca_dicotomica(diccionario, e, mid-1, x, ++comp);
+	else if (x > diccionario[mid]) return cerca_dicotomica(diccionario,mid+1, d, x, ++comp);
+}
+
+int main() {
+  clock_t start_s = clock();
+  int comp, bTrue, bFalse;
+  comp = bTrue = bFalse = 0;
+  string lineD, lineE;
+  vector <int> diccionario; // Tots els números del fitxer diccionario.txt
+  vector <int> entrada; // Tots els números del fitxer entrada.txt
+  vector <int> numComp; // Tots els números de les comparacions de cada element d'entrada
+  ifstream dicc ("diccionario.txt");
+  ifstream ent ("entrada.txt");
+  if (dicc and ent) {
+    /** Insercció dels valors del fitxers als corresponents vectors **/
+    clock_t entrada_1 = clock();
+    while (getline(dicc, lineD) and getline(ent, lineE)) {
+			string Text = lineD.c_str();
+			string Text2 = lineE.c_str();
+			int Number;
+			int Number2;
+			if ( ! (istringstream(Text) >> Number) ) Number = 0;
+			if ( ! (istringstream(Text2) >> Number2) ) Number2 = 0;
+    	diccionario.push_back(Number);
+    	entrada.push_back(Number2);
+    }
+	  while (getline(ent, lineE)) {
+			string Text = lineE.c_str();
+			int Number;
+			if ( ! (istringstream(Text) >> Number) ) Number = 0;
+			entrada.push_back(Number);
+		}
+    dicc.close();
+    ent.close();
+    clock_t entrada_2 = clock();
+
+
+
+    bloom_parameters parameters;
+
+    // How many elements roughly do we expect to insert?
+    parameters.projected_element_count = diccionario.size();
+
+    // Maximum tolerable false positive probability? (0,1)
+    parameters.false_positive_probability = 0.0001; // 1 in 10000
+
+    // Simple randomizer (optional)
+    parameters.random_seed = 0xA5A5A5A5;
+
+    parameters.compute_optimal_parameters();
+
+    bloom_filter filter(parameters);
+
+    for (int i = 0; i < diccionario. size(); ++i){
+    	filter.insert(diccionario[i]);
+    }
+
+    cout << "hellooo " << parameters.optimal_parameters.number_of_hashes <<  " "<< parameters.optimal_parameters.table_size <<endl;
+
+  /** Cerca Hash **/
+    clock_t hash_1 = clock();
+    bool b;
+    for (int i = 0; i < entrada.size(); ++i){
+      comp  = 0;
+    	b = filter.contains(entrada[i]);
+    	if (b) ++bTrue;
+    	else ++bFalse;
+    	numComp.push_back(comp);
+    }
+
+    /**  Numero de comparacions i average **/
+	comp = 0;
+	for (int i = 0; i < numComp.size(); ++i) comp += numComp[i];
+
+    clock_t hash_2 = clock();
+    clock_t stop_s=clock();
+    cout << "Temps total del programa: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << endl;
+    cout << "Temps de lectura dels fitxers: " << (entrada_2-entrada_1)/double(CLOCKS_PER_SEC)*1000 << endl;
+    cout << "Temps d'execució de l'algoritme de Cerca Dicotómica: " << (hash_2-hash_1)/double(CLOCKS_PER_SEC)*1000 << endl;
+    cout << "Quantitat de paraules que estaven al diccionari: " << bTrue << endl;
+    cout << "Quantitat de paraules que no estaven al diccionari: " << bFalse << endl;
+    cout << "Quantitat de comparacions fetes: " << comp << endl;
+    cout << "Mitjana aritmética de comparacions totals: " << comp / numComp.size() << endl;
+    cout << "Número total d'entrades diccionari: " << diccionario.size() << ", entrades text: " << entrada.size() << endl;
+
+  }
+  else cout << "Error de opertura de ficheros \n";
+
+
+
+}
